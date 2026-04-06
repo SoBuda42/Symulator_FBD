59103088903e149ae5b22980621a6e061e7beb4b
Majster <majster@budowa.pl>
2026-04-07 01:23:39 +0200

    Zadanie 4 nieskonczone

---
+++ Podstawowy_blok_funkcyjny.cpp
@@ -0,0 +1,38 @@
+#include "Podstawowy_blok_funkcyjny.h"
+#include "Interfejs_wejscia.h"
+
+Podstawowy_blok_funkcyjny::Podstawowy_blok_funkcyjny() {}
+
+Podstawowy_blok_funkcyjny::~Podstawowy_blok_funkcyjny()
+{
+    for(int i=0; i<wejscia.size(); i++) {delete wejscia[i];}
+    for(int i=0; i<wyjscia.size(); i++) {delete wyjscia[i];}
+}
+
+Interfejs_wejscia* Podstawowy_blok_funkcyjny::wez_wejscie(int ktore_wejscie) 
+{
+    if(ktore_wejscie>=0 && ktore_wejscie<wejscia.size()) {return wejscia[ktore_wejscie];}
+    return nullptr;
+}
+
+Interfejs_wyjscia* Podstawowy_blok_funkcyjny::wez_wyjscie(int ktore_wyjscie) 
+{
+    if(ktore_wyjscie>=0 && ktore_wyjscie<wyjscia.size()) {return wyjscia[ktore_wyjscie];}
+    return nullptr;
+}
+
+bool Podstawowy_blok_funkcyjny::czy_polaczyc_wejscie(Interfejs_polaczenia* nowy_kabel) 
+{
+    if (nowy_kabel==nullptr) return false;
+
+    for(auto gniazdo : wejscia)
+    {
+        if(gniazdo!=nullptr)
+        {
+            gniazdo->polacz(nowy_kabel);
+            return true;
+        }
+    }
+    return false;
+}
+
---
+++ Podstawowy_blok_funkcyjny.h
@@ -0,0 +1,60 @@
+#ifndef PODSTAWOWY_BLOK_FUNKCYJNY_H
+#define PODSTAWOWY_BLOK_FUNKCYJNY_H
+
+#include "Interfejs_bloku_funkcyjnego.h"
+#include <vector>
+
+class Interfejs_wejscia;
+class Interfejs_wyjscia;
+class Interfejs_polaczenia;
+
+/**
+ * @brief Uniwersalny kadlub maszyny (Klasa bazowa dla blokow funkcyjnych).
+ * * Zarzadza wbudowanymi gniazdami wejsciowymi i wyjsciowymi. Posiada gotowa infrastrukture
+ * do podpinania kabli, ktora bedzie dziedziczona przez konkretne maszyny liczace.
+ */
+class Podstawowy_blok_funkcyjny : public Interfejs_bloku_funkcyjnego   
+{
+    protected:
+        /** @brief Magazyn przechowujacy wskazniki na wbudowane gniazda wejsciowe maszyny. */
+        std::vector<Interfejs_wejscia*> wejscia;
+
+        /** @brief Magazyn przechowujacy wskazniki na wbudowane gniazda wyjsciowe maszyny. */
+        std::vector<Interfejs_wyjscia*> wyjscia;
+    public:
+        /**
+         * @brief Domyslny konstruktor.
+         * * Przygotowuje poczatkowy stan pamieci dla maszyny i jej magazynow.
+         */
+        Podstawowy_blok_funkcyjny();
+
+        /**
+         * @brief Wirtualny destruktor.
+         * * Gwarantuje poprawne zniszczenie wszystkich fizycznych gniazd, ktore byly
+         * wbudowane w ta maszyne (czyszczenie wektorow).
+         */
+        virtual ~Podstawowy_blok_funkcyjny();
+
+        /**
+         * @brief Zwraca pilota (wskaznik) do konkretnego gniazda wejsciowego.
+         * * @param ktore_wejscie Numer indeksu gniazda w wektorze.
+         * * @return Wskaznik na zadany Interfejs_wejscia lub nullptr, jesli taki indeks nie istnieje.
+         */
+        Interfejs_wejscia* wez_wejscie(int ktore_wejscie) override;
+
+        /**
+         * @brief Zwraca pilota (wskaznik) do konkretnego gniazda wyjsciowego.
+         * * @param ktore_wyjscie Numer indeksu gniazda w wektorze.
+         * * @return Wskaznik na zadany Interfejs_wyjscia lub nullptr, jesli taki indeks nie istnieje.
+         */
+        Interfejs_wyjscia* wez_wyjscie(int ktore_wyjscie) override;
+
+        /**
+         * @brief Przeszukuje gniazda i automatycznie podpina kabel do pierwszego wolnego.
+         * * @param polacz_wejscie Wskaznik na kabel (Interfejs_polaczenia), ktory chcemy wpiac.
+         * * @return True, jesli udalo sie fizycznie wpiac kabel. False w przypadku braku wolnych miejsc.
+         */
+        bool czy_polaczyc_wejscie(Interfejs_polaczenia* nowy_kabel);
+};
+
+#endif
